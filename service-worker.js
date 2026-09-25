6745be605e774dc0f612bcc2084e60d62670220a
Zdaman3030 <heyyzachary@gmail.com>
2026-09-25 03:59:10 -0400

    Refresh offline cache for suitability checker build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="christians-auto-repair-v2";
+const CACHE_NAME="christians-auto-repair-v3";
 const CORE_ASSETS=["./","./index.html","./styles.css","./script.js","./config.js","./favicon.svg","./manifest.webmanifest","./404.html"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS)).then(()=>self.skipWaiting()));
