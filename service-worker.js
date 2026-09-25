289b11f30fdac1aa7535fac9f6d96514d9794836
Zdaman3030 <heyyzachary@gmail.com>
2026-09-25 03:45:33 -0400

    Add offline-capable service worker

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME="christians-auto-repair-v1";
+const CORE_ASSETS=["./","./index.html","./styles.css","./script.js","./config.js","./favicon.svg","./manifest.webmanifest","./404.html"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+  if(request.method!=="GET")return;
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(
+    fetch(request).then(response=>{
+      if(response&&response.ok){
+        const copy=response.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));
+      }
+      return response;
+    }).catch(()=>caches.match(request).then(hit=>hit||caches.match("./index.html")))
+  );
+});
