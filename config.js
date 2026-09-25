3523158da5a6c3ade2fe153ac4166b8f1371d1c4
Zdaman3030 <heyyzachary@gmail.com>
2026-09-25 03:32:29 -0400

    Add config.js for Christian's Auto Repair

---
+++ config.js
@@ -0,0 +1,10 @@
+window.CAR_CONFIG = {
+  businessName: "Christian's Auto Repair",
+  phoneDisplay: "[Add business number]",
+  phoneHref: "",
+  smsHref: "",
+  emailDisplay: "[Add business email]",
+  emailHref: "",
+  serviceArea: "[Add service area]",
+  hours: "[Add business hours]"
+};
